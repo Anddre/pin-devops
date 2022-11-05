ff06ecb64651474774323ef07315f9e7d6ba08bb
Nicolas Sosa (nss) <nico77@gmail.com>
2022-11-05 16:39:28 -0300

    Instancia us-east-2 funcionando

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,10 +17,13 @@ resource "aws_key_pair" "key_pair" {
 # Crear una instancia ec2 con os ubuntu 20.04
 resource "aws_instance" "node" {
   instance_type          = "t2.micro" # free instance
-  ami                    = "ami-0d527b8c289b4af7f"
+# ami                    = "ami-0d527b8c289b4af7f"
+  ami                    = "ami-0066d036f9777ec38"
   key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
+
+ # iam_instance_profile = "${aws_iam_instance_profile.ec2iam_profile.name}"
 
   tags = {
     Name = "pin ec2"
@@ -37,3 +40,21 @@ resource "aws_instance" "node" {
 resource "aws_eip" "eip" {
   instance = aws_instance.node.id
 }
+
+
+# resource "aws_iam_instance_profile" "ec2iam_profile" {
+#   name  = "ec2iam_profile"
+#   role = ["${instance_role.name}"]
+# }
+
+# resource "aws_iam_policy" "policy" {
+#   name        = "ec2-admin-pin2"
+#   description = "Este es un rol para manejar ec2"
+#   policy      =  file("${path.root}/ec2/ec2admin.json")
+# }
+
+# resource "aws_iam_role" "instance" {
+#   name               = "instance_role"
+#   path               = "/system/"
+#   assume_role_policy = file("${path.root}/ec2/ec2admin.json")
+# }
--- ec2/userdata.tpl
+++ ec2/userdata.tpl
@@ -33,7 +33,8 @@ sudo apt-get update
 sudo apt-get install helm
 
 # aws IAM Authenticator
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+curl -o /home/ubuntu/aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x /home/ubuntu/aws-iam-authenticator
 
 ## Install Terraform
 
